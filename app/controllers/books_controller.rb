8238d1e1a19fab0ec7becfd84032f3bef15c956f
EC2 Default User <ec2-user@ip-172-31-39-245.ec2.internal>
2022-01-14 14:50:50 +0000

    [Add]form_with

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,13 @@
 class BooksController < ApplicationController
   def index
+    @books=Book.all
+    @book=Book.new
   end
 
   def create
-    render plain: params[:title, body]
+    @book=Book.new(book_params)
+    @book.save
+    redirect_to'/books/show'
   end
 
   def show
@@ -11,4 +15,10 @@ class BooksController < ApplicationController
 
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
+
